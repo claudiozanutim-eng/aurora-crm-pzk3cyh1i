379b382ad84818506a53bf72e0fc3b7d3359c289
Skip <skip@usecurling.com>
2026-06-23 16:52:55 -0300

    feat: Increase horizontal logo size by 80%

--- src/components/layout/AppSidebar.tsx
+++ src/components/layout/AppSidebar.tsx
@@ -31,9 +31,13 @@ export function AppSidebar({ className, onNavigate }: AppSidebarProps) {
 
   return (
     <div className={cn('flex h-full w-full flex-col bg-white border-r', className)}>
-      <div className="flex h-16 items-center px-6 border-b">
+      <div className="flex h-24 shrink-0 items-center px-6 border-b">
         <Link to="/" className="flex items-center" onClick={onNavigate}>
-          <img src={auroraLogoHorizontal} alt="Aurora CRM" className="h-10 w-auto object-contain" />
+          <img
+            src={auroraLogoHorizontal}
+            alt="Aurora CRM"
+            className="h-[72px] w-auto object-contain"
+          />
         </Link>
       </div>
 
